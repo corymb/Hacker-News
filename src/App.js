378f1c9fe059e26bad1ab9ef6fbc11b90ed02720
Dave Bush <github@david-bush.co.uk>
2017-01-16 10:09:25 +1300

    Integrated with Hacker News API.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search/'
+
+class ListView extends Component {
+  render() {
+    if (this.props.data) {
+      return (
+        <span> { 
+          this.props.data.hits.map((i) => (
+            <p key={i.objectID }>{i.title}</p>)
+          )}
+        </span>
+      )
+    }
+    else {
+      return <p>Loading...</p>
+    }
+  }
+}
+
 class App extends Component {
+
+  componentDidMount() {
+    this.fetchTopstories()
+  }
+
+  updateDisplay(result) {
+    this.results = result
+    this.setState(result)
+  }
+
+  fetchTopstories() {
+    fetch(API_ENDPOINT)
+      .then(response => response.json())
+      .then(result => this.updateDisplay(result));
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Hacker News</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ListView data={ this.results }/>
       </div>
     );
   }
